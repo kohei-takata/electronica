80623429258394caa016967145175759c967d42c
Kohei TAKATA <kt.koheitakata@gmail.com>
2015-12-08 23:59:59 +0900

    Hello electron

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+electron.crashReporter.start();
+
+let mainWindow = null;
+
+app.on('window-all-closed', () => {
+ app.quir();
+});
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({width: 800, height: 600});
+    mainWindow.loadURL(`file://${ __dirname}/index.html`);
+    mainWindow.webContents.openDevTools();
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+});
